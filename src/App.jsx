9d63ae6aecd5810ecb3da9ec0d41ac92745b8d61
henrique <henrique.nunesf@eaportal.org>
2023-08-17 13:40:52 -0300

    0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.3

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,23 @@ import { useState } from 'react'
 
 import './App.css'
 
-function App() {
+export function App() {
+
+  const [usuario, setUsuario] = useState("")
+  const [senha, setSenha] = useState("")
+
+  function handleUsuario(evento) {
+    setUsuario(evento.target.value)
+  }
+
+  function handleSenha(evento) {
+    setSenha(evento.target.value)
+  }
+
+  function handleSubmit(evento) {
+    alert(`Usuário: ${usuario} / Senha: ${senha}`)
+  }
+
   return (
     <>
       <header>
@@ -10,15 +26,15 @@ function App() {
       </header>
       
       <main>
-        <form action="">
+        <form action="" onSubmit={handleSubmit}>
           <div>
             <label htmlFor="usuario">Usuário</label>
-            <input type="text" id="usuario" placeholder='Digite seu usuário' />
+            <input type="text" id="usuario" placeholder='Digite seu usuário' onChange={handleUsuario}/>
           </div>
 
           <div>
             <label htmlFor="senha">Senha</label>
-            <input type="password" id="senha" placeholder='Digite sua senha'/>
+            <input type="password" id="senha" placeholder='Digite sua senha' onChange={handleSenha}/>
           </div>
           <button>Entrar</button>
         </form>
